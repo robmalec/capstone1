a41371b19ba82eba1a8063100f868461310ad842
robmalec <robmalec@umich.edu>
2019-10-18 13:19:03 -0400

    Looks like it's working in the most basic way

--- src/co/grandcircus/Capstone1.java
+++ src/co/grandcircus/Capstone1.java
@@ -1,4 +1,4 @@
-package co.grandcircus
+package co.grandcircus;
 
 import java.util.Scanner;
 import java.util.Vector;
@@ -7,31 +7,49 @@ public class Capstone1 {
 	public static Scanner scn = new Scanner(System.in);
 	public static void main(String[] args) {
 		StringBuilder s = new StringBuilder();
-		Vector<StringBuilder> strings = new Vector<StringBuilder>();
+		String[] stringArray = null;
 		char repeatChar = ' ';
 		do {
-			System.out.println("Type in something to be translated to Pig Latin:");
-			getInput(strings);
 			
-			
-			
-			
-		} while (repeatChar != 'n');
-	}
-	public static void getInput(Vector<StringBuilder> inputArr) {
-		StringBuilder temp = new StringBuilder();
-		StringBuilder nextLine;
-		Boolean done = false;
-		while (!done) {
-			nextLine = new StringBuilder(scn.next());
-			inputArr.add(nextLine);
-			if (inputArr.lastElement().toString().endsWith("\n")) {
-				done = true;
+			try {
+				System.out.println("Type in something to be translated to Pig Latin:");
+				
+				//Splitting up input by word
+				stringArray = scn.nextLine().split(" ");
+				
+				//Translating each word
+				for (int i = 0; i < stringArray.length; i++) {
+					stringArray[i] = toPigLatin(stringArray[i]);
+					System.out.print(stringArray[i] + " ");
+				}
+				System.out.println("");
+				
+				System.out.println("Would you like to translate another sentence? (y/n)");
+				repeatChar = getCheckRepeatChar();
+				
 			}
+			catch (Exception e) {
+				System.out.println("Fool.  You have failed.  Try again.");
+				repeatChar = 'y';
+			}
+			
+		} while (repeatChar ==  'y');
+	}
+	public static char getCheckRepeatChar() throws Exception {
+		char r = scn.nextLine().charAt(0);
+		if ((r != 'y') && (r != 'n')) {
+			throw new Exception();
 		}
-		
-		
-		
+		return r;
+	}
+	public static void getInput(String[] stringArray) {
+		stringArray = scn.nextLine().split(" ");
+	}
+	public static String toPigLatin(String input) {
+		char temp = input.charAt(0);
+		input = input.substring(1);
+		input+= temp + "ay";
+		return input;
 	}
 	public static StringBuilder toPigLatin(StringBuilder input) {
 		input.append(input.charAt(0));
